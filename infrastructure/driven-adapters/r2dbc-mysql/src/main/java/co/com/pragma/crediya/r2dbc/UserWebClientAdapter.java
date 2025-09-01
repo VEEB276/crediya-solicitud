3052d165ccca5d2b275f0f3d61f48f489dc83d69
vescobarb <dtic.dev22@uis.edu.co>
2025-08-31 20:06:04 -0500

    Se elimina import sin usar

--- infrastructure/driven-adapters/r2dbc-mysql/src/main/java/co/com/pragma/crediya/r2dbc/UserWebClientAdapter.java
+++ infrastructure/driven-adapters/r2dbc-mysql/src/main/java/co/com/pragma/crediya/r2dbc/UserWebClientAdapter.java
@@ -1,13 +1,6 @@
 package co.com.pragma.crediya.r2dbc;
 
 import co.com.pragma.crediya.gateways.UserGateway;
-import co.com.pragma.crediya.model.solicitud.Solicitud;
-import co.com.pragma.crediya.model.solicitud.gateways.SolicitudRepository;
-import co.com.pragma.crediya.r2dbc.entities.SolicitudEntity;
-import co.com.pragma.crediya.r2dbc.helper.ReactiveAdapterOperations;
-import org.reactivecommons.utils.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
